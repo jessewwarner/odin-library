2bb0ce32373168a2e8582ba9edf0811056c22479
Jesse Warner <jesse.w.warner@gmail.com>
2023-10-03 19:44:28 -0400

    Change object function to class

--- js/script.js
+++ js/script.js
@@ -13,11 +13,20 @@ const authorError = document.getElementById('author-empty');
 const pagesError = document.getElementById('pages-empty');
 
 // Book object
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read
+    }
 }
 
 //Empty the bookContainer and repopulate it with books from the local library
